7b0caf2e4b44e27426699d026803786d4208a043
WEIQ311 <1107438082@qq.com>
2016-10-07 09:45:38 +0800

    拦截器初步修改

--- src/main/java/com/jzpz/interceptor/WebSecurityConfig.java
+++ src/main/java/com/jzpz/interceptor/WebSecurityConfig.java
@@ -3,40 +3,60 @@ package com.jzpz.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by weiQiang on 2016/10/2.
  */
-//@Configuration
-//@EnableWebSecurity
-public class WebSecurityConfig{
-
-/**
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //配置Spring Security的Filter链
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+    //配置如何通过拦截器保护请求
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login.html")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+            .authorizeRequests()
+            .antMatchers(
+                    HttpMethod.GET,
+                    "/*.html",
+                    "/**/**/*.html",
+                    "/*.js",
+                    "/**/*.js",
+                    "/**/*.TTF",
+                    "/**/**/**/*.js",
+                    "/**/**/**/*.map",
+                    "/**/**/**/*.css"
+            ).permitAll()
+            .antMatchers("/user/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login.html")
+            .permitAll()
+            .and()
+            .rememberMe()
+            .tokenValiditySeconds(1000)
+            .key("spitterKey")
+            .and()
+            .logout()
+            .permitAll();
+        http.headers().disable();
     }
-
+    //配置user-detail
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //auth.inMemoryAuthentication().withUser("admin").password("bonc").roles("USER_ROLE");
+        //auth.userDetailsService(null);
     }
-*/
 }
